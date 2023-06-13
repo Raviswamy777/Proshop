c64431427b3e2c9a3c4a44e18c6f8e561f043ecb
Ravi <raviswamyp@gmail.com>
2023-06-13 16:13:23 +0530

    Updated Up TO Place order

---
+++ frontend/src/screens/OrderScreen.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Row, Col, ListGroup, Image, Card, Button } from 'react-bootstrap';
+//import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import {
+  useDeliverOrderMutation,
+  useGetOrderDetailsQuery,
+  useGetPaypalClientIdQuery,
+  usePayOrderMutation,
+} from '../slices/ordersApiSlice';
+
+const OrderScreen = () => {
+  return <div>OrderScreen</div>
+};
+
+export default OrderScreen;
